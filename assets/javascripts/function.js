75e2258f2521f53eb0a90e8a19c34d6da723b4d0
chinhlv91@gmail.com <chinhlv91@gmail.com>
2019-09-24 16:57:15 +0700

    Add assets

---
+++ assets/javascripts/function.js
@@ -0,0 +1,18 @@
+;(function ($) {
+  "use strict";
+
+  var body = $('body'),
+      bodyHeight = body.height(),
+      windowHeight = $(window).height(),
+      windowWidth = $(window).width();
+
+  var passItForm = {
+    init: function () {
+
+    },
+  }
+
+  $(document).ready(function () {
+    passItForm.init();
+  });
+})(window.jQuery);
